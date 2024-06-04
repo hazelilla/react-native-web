0d6d38310ebdfeadc5811059b3ba50a2e298848c
Elanur Lilai <elanur.lilai@popleads.com>
2024-06-04 09:59:14 +0200

    feat: react native web app

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {createRoot} from 'react-dom/client';
+import {Platform} from 'react-native';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (Platform.OS === 'web') {
+    const root = createRoot(document.getElementById('root'));
+    root.render(<App />);
+  }
