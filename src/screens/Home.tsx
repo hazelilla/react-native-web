5f989af587afb1ae7bfca2bb0b405c963721637f
Elanur Lilai <elanur.lilai@popleads.com>
2024-06-13 16:31:18 +0200

    feat: Home screen UI for both mobile and web

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,141 +1,98 @@
-import React, { useEffect } from 'react';
-import { ScrollView, View } from 'react-native';
-import { CircularProgressBar, Layout, Text, Button, Radio, CheckBox, CheckBoxProps } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
-import DateTimePicker from '../component/DateTimePicker';
-
-const useCheckboxState = (initialCheck = false): CheckBoxProps => {
-    const [checked, setChecked] = React.useState(initialCheck);
-    return { checked, onChange: setChecked };
-};
+import React, { useEffect, useRef, useState } from 'react';
+import { ScrollView, View, FlatList, Image, StyleSheet, Dimensions, Platform } from 'react-native';
+import { Layout, Text } from '@ui-kitten/components';
+import ProductItem from '../component/ProductItem';
 
 const HomeScreen = () => {
-    const [checked, setChecked] = React.useState(false);
-    const [progress, setProgress] = React.useState(0);
-    const primaryCheckboxState = useCheckboxState();
-    const successCheckboxState = useCheckboxState();
-    const infoCheckboxState = useCheckboxState();
-    const warningCheckboxState = useCheckboxState();
-    const dangerCheckboxState = useCheckboxState();
-    const basicCheckboxState = useCheckboxState();
-    const controlCheckboxState = useCheckboxState();
+    const flatListRef = useRef<FlatList | null>(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const { width: screenWidth } = Dimensions.get('window');
+    const recommendedItems = Array.from({ length: 50 }, (_, index) => ({ key: `product-${index}` }));
+
+    const carouselImages = [
+        require('../assets/images/carousel1.png'),
+        require('../assets/images/carousel2.png'),
+        require('../assets/images/carousel3.png'),
+    ];
+
+    const renderVerticalRecommendedItem = ({ item }) => (
+        <ProductItem buttonRemoval={true} isVertical={true} />
+    );
+
+    const renderImage = ({ item }) => (
+        <Image
+            source={item}
+            style={{
+                height: 300,
+                width: screenWidth,
+                borderTopRightRadius: Platform.OS !== 'web' ? 0 : 300,
+                borderBottomRightRadius: Platform.OS !== 'web' ? 0 : 300,
+            }}
+            resizeMode="cover"
+        />
+    );
+
+    const renderIndicator = (index) => {
+        const backgroundColor = index === currentIndex ? '#333333' : '#CCCCCC';
+        return <View style={[styles.indicator, { backgroundColor }]} key={index} />;
+    };
+
+    const scrollToNextItem = () => {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToIndex({
+                index: (currentIndex + 1) % carouselImages.length,
+                animated: true,
+            });
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselImages.length);
+        }
+    };
 
     useEffect(() => {
-        setProgress(0);
-    }, [])
+        const interval = setInterval(scrollToNextItem, 3000);
 
-    const increaseProgress = () => {
-        setProgress(progress + 10);
-    };
+        return () => {
+            clearInterval(interval);
+        };
+    }, [currentIndex]);
 
     return (
         <Layout style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollView} showsVerticalScrollIndicator={false}>
-                <Layout style={{ marginVertical: 50, alignItems: 'center' }}>
+                <Layout style={{ alignItems: 'center' }}>
+                    <FlatList
+                        ref={flatListRef}
+                        data={carouselImages}
+                        horizontal
+                        pagingEnabled
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={renderImage}
+                        keyExtractor={(item, index) => index.toString()}
+                        style={{ width: screenWidth }}
+                        contentContainerStyle={styles.carouselContentContainer}
+                        initialScrollIndex={0}
+                        getItemLayout={(data, index) => ({
+                            length: screenWidth,
+                            offset: screenWidth * index,
+                            index,
+                        })}
+                    />
+                    <View style={styles.indicatorContainer}>
+                        {carouselImages.map((_, index) => renderIndicator(index))}
+                    </View>
 
-                    <Text category='h1' >Welcome to Home</Text>
-                    <Text category='s1' >Explore what's new</Text>
-
-                    <Button
-                        style={styles.button}
-                        appearance='filled'
-                    >
-                        FILLED
-                    </Button>
-
-                    <Button
-                        style={styles.button}
-                        appearance='outline'
-                    >
-                        OUTLINE
-                    </Button>
-
-                    <Button
-                        style={styles.button}
-                        appearance='ghost'
-                    >
-                        GHOST
-                    </Button>
-
-                    <Button
-                        style={{ backgroundColor: 'black' }}
-                        appearance='filled'
-                    >
-                        COLORED
-                    </Button>
-
-                    <Radio
-                        checked={checked}
-                        onChange={nextChecked => setChecked(nextChecked)}
-                        style={{ marginTop: 20 }}
-                    >
-                        {`Checked: ${checked}`}
-                    </Radio>
-
-                    <CircularProgressBar progress={progress} style={{ marginVertical: 20 }} />
-
-                    <Button onPress={increaseProgress} style={{ marginBottom: 30 }}>Increase Progress</Button>
-
-                    <Layout level='3' style={{ padding: 10 }}>
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='primary'
-                            {...primaryCheckboxState}
-                        >
-                            Primary
-                        </CheckBox>
-
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='success'
-                            {...successCheckboxState}
-                        >
-                            Success
-                        </CheckBox>
-
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='info'
-                            {...infoCheckboxState}
-                        >
-                            Info
-                        </CheckBox>
-
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='warning'
-                            {...warningCheckboxState}
-                        >
-                            Warning
-                        </CheckBox>
-
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='danger'
-                            {...dangerCheckboxState}
-                        >
-                            Danger
-                        </CheckBox>
-
-                        <CheckBox
-                            style={styles.checkbox}
-                            status='basic'
-                            {...basicCheckboxState}
-                        >
-                            Basic
-                        </CheckBox>
-
-                        <View style={styles.controlContainer}>
-                            <CheckBox
-                                style={styles.checkbox}
-                                status='control'
-                                {...controlCheckboxState}
-                            >
-                                Control
-                            </CheckBox>
+                    <View style={{ marginTop: 20, flex:5, width: '98%'}}>
+                        <Text category="h1">Deals</Text>
+                        <View style={[styles.boxStyle]}>
+                            <FlatList
+                                data={recommendedItems}
+                                renderItem={renderVerticalRecommendedItem}
+                                keyExtractor={(item, index) => index.toString()}
+                                horizontal={true}
+                                showsHorizontalScrollIndicator={false}
+                                style={{ width: '100%' }}
+                            />
                         </View>
-                    </Layout>
-                    <DateTimePicker />
+                    </View>
                 </Layout>
             </ScrollView>
         </Layout>
@@ -148,21 +105,26 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        padding: 16,
     },
-    button: {
-        margin: 2,
-        marginTop: 15
+    carouselContentContainer: {
+        flexGrow: 1,
     },
-    checkbox: {
-        margin: 2,
+    indicatorContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10,
     },
-    controlContainer: {
-        borderRadius: 4,
-        margin: 2,
-        padding: 6,
-        backgroundColor: '#3366FF',
-    }
+    indicator: {
+        width: 20,
+        height: 3,
+        marginHorizontal: 2,
+        borderRadius: 2,
+    },
+    boxStyle: {
+        backgroundColor: "#F7F7F7",
+        borderRadius: 10,
+        padding: 10,
+    },
 });
 
 export default HomeScreen;
