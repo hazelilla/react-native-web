9979515173e1553f132d521d3a34ce1a3a2741e8
Elanur Lilai <elanur.lilai@popleads.com>
2024-06-25 17:15:10 +0200

    feat: Google maps for web and mobile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Layout, Text, Button } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
+declare global {
+  interface Window {
+    initMap: () => void;
+    google: any;
+  }
+}
+
 const ProfileScreen = () => {
+  {Platform.OS === 'web' &&
+    useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.initMap = () => {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+          center: { lat: 41.7979, lng: 20.9082 },
+          zoom: 8,
+        });
+      };
+
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCsiO74x3jCBecgqs585kAyV7E_TDc17ZM&callback=initMap`;
+      script.async = true;
+      document.head.appendChild(script);
+    }
+  }, []);}
+
   return (
     <Layout style={styles.container}>
       <Text category='h1'>Profile Screen</Text>
-      <Text category='p1'>
-        This is the second screen of your app. You can add more content here.
-      </Text>
 
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      {Platform.OS === 'web' ?
+        <div id="map" style={styles.mapWeb}></div>
+        :
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      }
 
     </Layout>
   );
@@ -31,12 +56,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
-    backgroundColor: '#FFFFFF', 
+    backgroundColor: '#FFFFFF',
   },
   map: {
     width: '100%',
     height: '40%',
   },
+  mapWeb:{
+    width: '60%',
+    height: '90%'
+  }
 });
 
 export default ProfileScreen;
